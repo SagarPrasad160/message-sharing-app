fc2db2e9ed0a14db86858e78174b3471f7471809
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-15 18:56:27 +0530

    add setup

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+const hash = window.location.hash;
+
+const message = atob(hash.slice(1));
+
+if (message) {
+  // show the message
+  document.querySelector("#message-show").classList.remove("hide");
+  document.querySelector("h1").textContent = message;
+
+  // hide the message form
+  document.querySelector("#message-form").classList.add("hide");
+}
+
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
+
   const input = document.querySelector("input");
-  console.log(input.value);
+
+  const linkInput = document.querySelector("#link-input");
+  const encrypted = btoa(input.value);
+
+  document.querySelector("#message-link").classList.remove("hide");
+  document.querySelector("#message-form").classList.add("hide");
+
+  // get the encrypted hash message with url  to the linkinput value
+  linkInput.value = `${window.location}#${encrypted}`;
+  //selet the link input
+  linkInput.focus();
+  linkInput.select();
 });
